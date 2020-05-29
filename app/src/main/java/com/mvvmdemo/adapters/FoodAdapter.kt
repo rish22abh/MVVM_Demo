a1294721d8c583040e3258c8ad0d2532380835d0
Rishabh <rishabhgupta@mobikasa.com>
2020-05-30 00:05:57 +0530

    fixed layout issue

--- app/src/main/java/com/mvvmdemo/adapters/FoodAdapter.kt
+++ app/src/main/java/com/mvvmdemo/adapters/FoodAdapter.kt
@@ -3,14 +3,16 @@ package com.mvvmdemo.adapters
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.mvvmdemo.R
 import com.mvvmdemo.databinding.ItemFoodBinding
 import com.mvvmdemo.model.Recipes
 
 class  FoodAdapter(private val mContext: Context, private var mList:ArrayList<Recipes>): RecyclerView.Adapter<FoodAdapter.ViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemFoodBinding = ItemFoodBinding.inflate(LayoutInflater.from(mContext))
+        val itemFoodBinding:ItemFoodBinding = DataBindingUtil.inflate(LayoutInflater.from(mContext),R.layout.item_food, parent, false)
         return ViewHolder(itemFoodBinding)
     }
 
